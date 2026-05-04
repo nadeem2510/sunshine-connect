f6ba427eeaeedc4b2c9186f2a9c9e3c494e4c666
nadeem2510 <nadeem2510@gmail.com>
2026-05-04 20:20:22 +0530

    Fix: convert PostCSS and Tailwind configs from ESM to CommonJS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
@@ -22,4 +22,5 @@ export default {
     },
   },
   plugins: [],
-};
+}
+
